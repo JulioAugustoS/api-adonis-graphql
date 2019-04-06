dca247a2dc29a9c24a1b1f384cb8d89108c2700b
Julio Augusto <julio@lapse.com.br>
2019-04-06 00:11:43 -0300

    Alterado porta pg

--- config/database.js
+++ config/database.js
@@ -77,7 +77,7 @@ module.exports = {
     client: 'pg',
     connection: {
       host: Env.get('DB_HOST', DATABASE_URL.host),
-      port: Env.get('DB_PORT', ''),
+      port: Env.get('DB_PORT', 5432),
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
       database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
